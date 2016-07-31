a51dd12a3cd03e4451d2dc81df375d4f13d90a8e
Oyami <hhx.xxm@gmail.com>
2016-07-31 18:07:11 +0800

    sprite2D

--- dx4s.h
+++ dx4s.h
@@ -1,6 +1,7 @@
 #pragma once
 #define DX4S_VERSION 2
 #define WIN32_LEAN_AND_MEAN  //不编译MFC模块,令生成的模块更小,运行更快
+#include <string>
 #include <Windows.h>
 #include <d3d11.h>
 #include <D3DX11.h>
--- utils/utils.h
+++ utils/utils.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "../dx4s.h"
+#include "sprite2d/sprite2d.h"
 
 namespace dx4s {
-	class utils {
+	class tools {
 	public:
 		static bool ShaderCompile(const char *Filename,
+								  const char *entryPoint,
+								  const char *proFile,
+								  ID3DBlob **buffer,
+								  UINT Flag = D3DCOMPILE_ENABLE_STRICTNESS);
+
+		static bool ShaderCompileFromString(const char *SrcString,
 								  const char *entryPoint,
 								  const char *proFile,
 								  ID3DBlob **buffer,
